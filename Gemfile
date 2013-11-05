12b0b9bc33fa02f363e35f0093e783ec2006fdd2
Matt Sayre <mlsayre@gmail.com>
2013-11-04 16:40:16 -0800

    shakespeare quote

--- Gemfile
+++ Gemfile
@@ -1,4 +1,5 @@
 source 'https://rubygems.org'
 
+gem 'speaker'
 # Specify your gem's dependencies in rulers.gemspec
 gemspec
---
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -0,0 +1,5 @@
+class QuotesController < Rulers::Controller
+  def a_quote
+    "There is nothing either good or bad"
+  end
+end
