30f01a444ee0d83e0fb9c851cbf5370ba7a4a26d
Matt Sayre <mlsayre@gmail.com>
2013-11-06 15:32:49 -0800

    chapter 5 complete

--- best_quotes/app/controllers/quotes_controller.rb
+++ best_quotes/app/controllers/quotes_controller.rb
@@ -1,4 +1,9 @@
 class QuotesController < Rulers::Controller
+  def index
+    quotes = FileModel.all
+    render :index, :quotes => quotes
+  end
+
   def exception
     raise "It's a bad one!"
   end
@@ -6,4 +11,20 @@ class QuotesController < Rulers::Controller
   def a_quote
     render :a_quote, :noun => :winking
   end
+
+  def quote_1
+    quote_1 = Rulers::Model::FileModel.find(1)
+    render :quote, :obj => quote_1
+  end
+
+  def new_quote
+    attrs = {
+      "submitter" => "web user",
+      "quote" => "A picture is worth a thousand pigeons.",
+      "attribution" => "Meh"
+    }
+    m = FileModel.create attrs
+    render :quote, :obj => m
+  end
+
 end
